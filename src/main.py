05811da0d1143d7eef414b42b0ebf33c51e2384d
malickGB <malick.grandbois@gmail.com>
2021-09-29 19:55:06 +0200

    initial commit

--- src/main.py
+++ src/main.py
@@ -1,21 +1,33 @@
 import os
 from discord.ext import commands
+from dotenv import load_dotenv
 
+
+token = os.getenv('BOT_TOKEN')
+print(token)
 bot = commands.Bot(
     command_prefix="!",  # Change to desired prefix
     case_insensitive=True  # Commands aren't case-sensitive
 )
 
-bot.author_id = 0000000  # Change to your discord id!!!
+bot.author_id = 'malick#5960'  # Change to your discord id!!!
 
 @bot.event
 async def on_ready():  # When the bot is ready
     print("I'm in")
     print(bot.user)  # Prints the bot's username and identifier
 
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+
+    if message.content.startswith('$name'):
+        await message.channel.send('Hello '+ message.author + '!')
+
+
 @bot.command()
 async def pong(ctx):
     await ctx.send('pong')
-
-token = "<MY_SUPER_TOKEN>"
+token= "ODkyODIxODA0ODY1OTc4NDI4.YVSfGg.PjaWzksYxURpqjmxeceM9U6u1zQ"
 bot.run(token)  # Starts the bot
