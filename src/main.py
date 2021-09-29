b39d6f8edc0b3ef1cf8180a32763001904a6b762
jacreed <paul.mesinele@epita.fr>
2021-09-29 20:22:59 +0200

    function name

--- src/main.py
+++ src/main.py
@@ -10,24 +10,45 @@ bot = commands.Bot(
     case_insensitive=True  # Commands aren't case-sensitive
 )
 
-bot.author_id = 'malick#5960'  # Change to your discord id!!!
+bot.author_id = 'paulmes#7230'  # Change to your discord id!!!
 
 @bot.event
 async def on_ready():  # When the bot is ready
     print("I'm in")
     print(bot.user)  # Prints the bot's username and identifier
+    print(bot)
 
 @bot.event
 async def on_message(message):
     if message.author == bot.user:
         return
 
-    if message.content.startswith('$name'):
-        await message.channel.send('Hello '+ message.author + '!')
+    if message.content.startswith('!name'):
+        await message.channel.send("Hello " + message.name + "!")
+
+    if message.content.startswith('!count'):
+        await message.channel.send("Hello " + message.name + "!")
+
+    if message.content.startswith('!admin'):
+        await message.channel.send("Hello " + message.name + "!")
+
+    if message.content.startswith('!mute'):
+        await message.channel.send("Hello " + message.name + "!")
+
+    if message.content.startswith('!ban'):
+        await message.channel.send("Hello " + message.name + "!")
+
+    if message.content.startswith('!xkcd'):
+        await message.channel.send("Hello " + message.name + "!")
+
+    if message.content.startswith('!poll'):
+        await message.channel.send("Hello " + message.name + "!")
+    
+    if message.content.startswith('!tictactoe'):
+        await message.channel.send("Hello " + message.name + "!")
 
 
 @bot.command()
 async def pong(ctx):
     await ctx.send('pong')
-token= "ODkyODIxODA0ODY1OTc4NDI4.YVSfGg.PjaWzksYxURpqjmxeceM9U6u1zQ"
 bot.run(token)  # Starts the bot
